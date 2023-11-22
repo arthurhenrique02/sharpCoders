850b5af801abdd58170e889018ca10c69f3e605d
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-11-22 20:28:28 -0300

    created first TS class

---
+++ typescript/oop.tsx
@@ -0,0 +1,12 @@
+class Person {
+    // attributes
+    // it`s not needed to put 'let' or something else
+    // before attribute`s declaration
+    person_name:string;
+    age:number;
+    city:string
+}
+
+
+// create instance
+let person = new Person();
