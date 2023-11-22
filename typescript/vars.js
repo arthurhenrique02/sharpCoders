9d15d2b9c4e0708ba6400b10dae4edd62a382c67
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-11-22 19:58:54 -0300

    Seen how to declare variables in TS

---
+++ typescript/vars.js
@@ -0,0 +1,7 @@
+// standard vars
+var _name = "Rayssa";
+var age = 21;
+var sutdy = true;
+// a var that you don`t know the type
+var address = "Meu coracao";
+console.log(_name);
---
+++ typescript/vars.tsx
@@ -0,0 +1,9 @@
+// standard vars
+let person_name:string = "Rayssa";
+let age:number = 21;
+let sutdy:boolean = true;
+
+// a var that you don`t know the type
+let address:any = "Meu coracao";
+
+console.log(person_name);
