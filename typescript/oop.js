25414f91955347f414a2a62a953cee28d2a58ace
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-11-22 21:03:31 -0300

    private and public funcs

--- typescript/oop.js
+++ typescript/oop.js
@@ -1,8 +1,15 @@
 var Person = /** @class */ (function () {
+    // tried to overload
+    //constructor();
     function Person(p_name, age) {
         this.person_name = p_name;
         this.age = age;
+        this.print_name();
     }
+    // don`t use the word `function` inside a class
+    Person.prototype.print_name = function () {
+        console.log("Seu nome \u00E9: ".concat(this.person_name));
+    };
     return Person;
 }());
 // create instance
--- typescript/oop.tsx
+++ typescript/oop.tsx
@@ -5,17 +5,29 @@ class Person {
     person_name:string;
     age:number;
     city:string
+    // private attribut
+    private cpf:string;
 
     // tried to overload
     //constructor();
-    constructor(p_name:string, age:number){
+    constructor(p_name:string, age:number, cpf:string){
         this.person_name =  p_name;
-        this.age = age
+        this.age = age;
+        this.cpf = cpf;
+
+        this.print_name();
+    }
+
+    // don`t use the word `function` inside a class
+    private print_name(){
+        console.log(`Seu nome é: ${this.person_name}\n` +
+                    `cpf: ${this.cpf}`);
+        
     }
 }
 
 
 // create instance
-let person = new Person("Arthur", 21);
+let person = new Person("Arthur", 21, "1234");
 
 console.log(person.person_name, person.age);
