ebd099c737e10d13451dac69ab973064df74bdd7
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-11-29 20:19:35 -0300

    understandig ngIf

--- angular/projetoAula01/src/app/second-component/second-component.component.ts
+++ angular/projetoAula01/src/app/second-component/second-component.component.ts
@@ -1,13 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-second-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './second-component.component.html',
   styleUrl: './second-component.component.css'
 })
 export class SecondComponentComponent {
   // attribute to show text
-  // showText =
+  showText:boolean = false;
 }
