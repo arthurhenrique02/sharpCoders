16ad431229dbaeb3c079b6f8219d50187abb8eca
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-12-04 20:33:03 -0300

    seen about reactive form

--- angular/projetoAula01/src/app/fifth-component/fifth-component.component.ts
+++ angular/projetoAula01/src/app/fifth-component/fifth-component.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-fifth-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './fifth-component.component.html',
   styleUrl: './fifth-component.component.css'
 })
 export class FifthComponentComponent {
+  // create new form
+  forms = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
 
+  submit_form():void {
+    
+  }
 }
