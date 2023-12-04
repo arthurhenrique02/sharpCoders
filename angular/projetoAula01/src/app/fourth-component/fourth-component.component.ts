d1f14cfbc9f41376c78108efd5bdecd23e834ecb
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-12-04 20:14:23 -0300

    classic way to use Angular forms (ngModel)

--- angular/projetoAula01/src/app/fourth-component/fourth-component.component.ts
+++ angular/projetoAula01/src/app/fourth-component/fourth-component.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-fourth-component',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ],
   templateUrl: './fourth-component.component.html',
   styleUrl: './fourth-component.component.css'
 })
 export class FourthComponentComponent {
+  text:string = "";
+
+  // vars to store form data
+  email:string = "";
+  password:string = "";
 
 }
