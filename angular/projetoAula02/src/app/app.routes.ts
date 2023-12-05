fc1db5c827c5127a0ffbaac4131259ed5b2c1b76
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-12-04 21:02:42 -0300

    Seen how to change routes using the routes module and RouteLink

--- angular/projetoAula02/src/app/app.routes.ts
+++ angular/projetoAula02/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Pg01Component } from './pg01/pg01.component';
+import { Pg02Component } from './pg02/pg02.component';
+import { Pg03Component } from './pg03/pg03.component';
+import { Pg404Component } from './pg404/pg404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "pg1", component: Pg01Component},
+    {path: "pg2", component: Pg02Component},
+    {path: "pg3", component: Pg03Component},
+    // redirectTo is a param to redirect to an existing route
+    // in this case, when the user access the root (/)
+    // he`ll be redirected to the pg1 route
+    {path:'', redirectTo:"/pg1", pathMatch:"full"},
+    // path: "**" tells to Angular the route don`t exists
+    {path:"**", component:Pg404Component}
+
+];
