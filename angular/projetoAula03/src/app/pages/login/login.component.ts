674785b30770fb5b7250098fdc78bf718a056b43
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-12-06 20:24:11 -0300

    seen how to use validators

--- angular/projetoAula03/src/app/pages/login/login.component.ts
+++ angular/projetoAula03/src/app/pages/login/login.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+    // create form instance form
+    form = new FormGroup({
+      // get email and password
+      // add required to this fields
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    });
 }
