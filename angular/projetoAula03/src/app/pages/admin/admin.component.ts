19e2a60e33336159bb6142a7d234ab001eaffc5d
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-12-06 20:56:22 -0300

    created a simple HTML to admin page

--- angular/projetoAula03/src/app/pages/admin/admin.component.ts
+++ angular/projetoAula03/src/app/pages/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -9,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AdminComponent {
 
+  // override constructor
+  constructor(private router: Router) {}
+
+  exit():void {
+    // remove email from local storage
+    localStorage.removeItem("email")
+    // redirect to login
+    this.router.navigateByUrl('/login');
+  }
 }
