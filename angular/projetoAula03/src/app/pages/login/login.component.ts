87bd354fd0a068fc3cf85afdadb55a8236313410
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-12-06 20:48:54 -0300

    added auth to login

--- angular/projetoAula03/src/app/pages/login/login.component.ts
+++ angular/projetoAula03/src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormGroup, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,8 @@ import { FormGroup, ReactiveFormsModule, FormControl, Validators } from '@angula
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+    // override constructor
+    constructor(private router: Router) {}
 
     // create form instance form
     form = new FormGroup({
@@ -18,4 +21,19 @@ export class LoginComponent {
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', Validators.required)
     });
+
+    authenticate():void {
+      // get email and password from form
+      const { email, password } = this.form.value;
+      // check if email and password are correct
+      if (email === 'arthur@email.com' && password === '123'){
+        // add email to local storage
+        localStorage.setItem("email", email)
+        // redirect to admin
+        this.router.navigateByUrl('/admin');
+      }else {
+        alert('Email ou senha incorretos')
+      }
+
+    }
 }
