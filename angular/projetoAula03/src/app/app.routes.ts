fc11e6eb66276609981835a024e62f145a1baa22
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-12-06 20:23:56 -0300

    visto autheticators

--- angular/projetoAula03/src/app/app.routes.ts
+++ angular/projetoAula03/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { AdminComponent } from './pages/admin/admin.component';
+import { authenticateGuard } from './auth/authenticate.guard';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent},
-    {path: "admin", component: AdminComponent},
+    {path: "admin", component: AdminComponent, canActivate: [authenticateGuard,]},
     {path: "", redirectTo: "/login", pathMatch: "full"}
 ];
--- angular/projetoAula03/src/app/auth/authenticate.guard.ts
+++ angular/projetoAula03/src/app/auth/authenticate.guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const authenticateGuard: CanActivateFn = (route, state) => {
+  // create router
+  let router = new Router();
+
+  // validate
+  // if user is not logged in, redirect to login page
+  if (localStorage.getItem('email') == undefined) {
+    router.navigateByUrl('/login');
+  }
+
+  // enable access by default
   return true;
 };
