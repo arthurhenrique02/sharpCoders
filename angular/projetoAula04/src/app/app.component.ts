042b04b920a97cef4726415558b8d8001d526362
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-12-11 20:45:01 -0300

    created BR address interface

--- angular/projetoAula04/src/app/app.component.ts
+++ angular/projetoAula04/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { PostsComponent } from './components/posts/posts.component';
+import { AddressesComponent } from './components/addresses/addresses.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PostsComponent],
+  imports: [CommonModule, RouterOutlet, PostsComponent, AddressesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
