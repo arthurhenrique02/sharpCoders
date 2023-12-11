bd05f5372ffd6a37da8a78c8f6404de4843eff16
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-12-11 20:55:41 -0300

    added logic to get the address

--- angular/projetoAula04/src/app/components/addresses/addresses.component.ts
+++ angular/projetoAula04/src/app/components/addresses/addresses.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AddressesService } from '../../services/addresses.service';
 
 @Component({
   selector: 'app-addresses',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './addresses.component.html',
   styleUrl: './addresses.component.css'
 })
 export class AddressesComponent {
 
+  //overriding the constructor
+  constructor(private addressService:AddressesService){}
+
+  cep:string = '';
+
+  getAddress():void{
+    this.addressService.getAdress(this.cep).subscribe((data)=>{
+      console.log(data);
+    });
+  }
 }
--- angular/projetoAula04/src/app/services/addresses.service.ts
+++ angular/projetoAula04/src/app/services/addresses.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Address } from '../../models/address';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddressesService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  // return the address
+  getAdress(cep:string):Observable<Address>{
+    return this.http.get<Address>(`https://viacep.com.br/ws/${cep}/json/`);
+  }
 }
